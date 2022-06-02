4562478510219bde76297b0eee9b634686009f0b
Quoc Luong <luongd872@gmail.com>
2022-06-02 15:53:33 +0700

    setup Layout

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,18 @@
+import Header from '../components/Header'
+import Sidebar from './Sidebar'
+
+
+
+export default function DefaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <Sidebar />
+                <div className="content">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
